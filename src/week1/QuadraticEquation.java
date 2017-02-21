3ad2c864f3ab8be9128e2cd27543d5c4853b487e
Grabets <grabetsb@gmail.com>
2017-02-21 23:11:32 +0200

    bug input fix

--- src/week1/QuadraticEquation.java
+++ src/week1/QuadraticEquation.java
@@ -8,16 +8,46 @@ import java.io.InputStreamReader;
  * Created by bogdan on 2/20/2017.
  */
 public class QuadraticEquation {
-
-
-    public static void main(String[] args) throws IOException {
+    //Input valid value
+    private static double inputMult () throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        double a = Double.parseDouble(reader.readLine());
-        double b = Double.parseDouble(reader.readLine());
-        double c = Double.parseDouble(reader.readLine());
+        boolean isExit=false;
+        String s;
+        do {
+            if (isExit)
+                System.out.println("Error! Please enter double value!");
+            s = reader.readLine();
+            if (doubleCheck(s)){
+                return Double.parseDouble(s);
+            }
+            else {
+                System.out.println("Error! Please enter double value!");
+                s = reader.readLine();
+                isExit = true;
+            }
+        } while (isExit);
+        return Double.parseDouble(s);
+    }
+    //checks for valid value
+    private static boolean doubleCheck(String s){
+        try {
+            Double.parseDouble(s);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
 
+    private static void solutionQE() throws IOException {
+        System.out.println("Please enter coefficients(a, b, c) in double value for quadratic equation ax*x+bx+c=0");
+        System.out.print("a= ");
+        double a = inputMult();
+        System.out.print("b= ");
+        double b = inputMult();
+        System.out.print("c= ");
+        double c = inputMult();
         double disc = Math.pow(b,2)-4*a*c;
-        System.out.println(disc);
         System.out.println("Result:");
 
         if (disc > 0){
@@ -28,8 +58,10 @@ public class QuadraticEquation {
             System.out.println("x1=x2= "+((0-b)/(2*a)));
         else
             System.out.println("Discriminant is negative, then there are no real roots.");
-
-
-
     }
+
+    public static void main(String[] args) throws IOException{
+        solutionQE();
+    }
+
 }

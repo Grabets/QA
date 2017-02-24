30512218bbc6be1def583c24f73f91d5a23c062a
Grabets <grabetsb@gmail.com>
2017-02-24 22:43:04 +0200

    revision 2

--- src/week1/QuadraticEquation.java
+++ src/week1/QuadraticEquation.java
@@ -8,29 +8,23 @@ import java.io.InputStreamReader;
  * Created by bogdan on 2/20/2017.
  */
 public class QuadraticEquation {
-    static int count = 1;
     //Input valid value
-    private static double inputMult () throws IOException{
+    private static double ReadNumberFromConsole () throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         boolean isExit=false;
         String s;
         //work until input valid value
         do {
             s = reader.readLine();
-            if (doubleCheck(s))
+            if (IsValid(s))
                 return Double.parseDouble(s);
         } while (!isExit);
         return Double.parseDouble(s);
     }
     //checks for valid value
-    private static boolean doubleCheck(String s){
+    private static boolean IsValid(String s){
         try {
             double x =  Double.parseDouble(s);
-            if (x==0.0 && count==1)
-            {
-                System.out.println("Division by zero! Please enter double value which not equal zero!");
-                return false;
-            }
             return true;
         }
         catch (NumberFormatException e){
@@ -39,30 +33,34 @@ public class QuadraticEquation {
         }
     }
 
-    private static void solutionQE() throws IOException {
+    private static void SolutionQE() throws IOException {
         System.out.println("Please enter coefficients(a, b, c) in double value for quadratic equation ax*x+bx+c=0");
         System.out.print("a= ");
-        double a = inputMult();
-        count--;
+        double a = ReadNumberFromConsole();
         System.out.print("b= ");
-        double b = inputMult();
+        double b = ReadNumberFromConsole();
         System.out.print("c= ");
-        double c = inputMult();
+        double c = ReadNumberFromConsole();
         double disc = Math.pow(b,2)-4*a*c;
         System.out.println("Result:");
-
-        if (disc > 0){
-            System.out.println("x1= "+((0-b+Math.sqrt(disc))/(2*a)));
-            System.out.println("x2= "+((0-b-Math.sqrt(disc))/(2*a)));
+        if (a==0&b!=0)
+        {
+            System.out.println("x="+(0-c)/b);
         }
-        else if (disc==0)
-            System.out.println("x1=x2= "+((0-b)/(2*a)));
         else
-            System.out.println("Discriminant is negative, then there are no real roots.");
+            if (disc > 0&&a!=0){
+                System.out.println("x1= "+((0-b+Math.sqrt(disc))/(2*a)));
+                System.out.println("x2= "+((0-b-Math.sqrt(disc))/(2*a)));
+            }
+            else
+                if (disc==0&&a!=0)
+                    System.out.println("x1=x2= "+((0-b)/(2*a)));
+            else
+                System.out.println("Discriminant is negative, then there are no real roots.");
     }
 
     public static void main(String[] args) throws IOException{
-        solutionQE();
+        SolutionQE();
     }
 
 }

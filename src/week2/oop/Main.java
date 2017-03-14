52a2030bf7ca9335c934a662e6287a1ff6eacd1a
Grabets <grabetsb@gmail.com>
2017-03-15 00:18:58 +0200

    add parser class

--- src/week2/oop/Main.java
+++ src/week2/oop/Main.java
@@ -10,6 +10,11 @@ public class Main {
         Reader reader = new Reader();
         //reads from file
         List<String> listOfEquation = reader.readFromFile("src/week2/oop/data.txt");
+        Parser parser = new Parser();
+        for (String s:
+             listOfEquation ) {
+            System.out.println(parser.parse(s));
+        }
     }
 
 }
--- src/week2/oop/Parser.java
+++ src/week2/oop/Parser.java
@@ -1,19 +1,50 @@
 package week2.oop;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Created by bogdan on 3/13/2017.
  */
 public class Parser {
-    private static final String QUADRAICE_EQUATION = "";
-    private static final String LINEAR_EQUATION = "";
 
-    private void parse(String s){
 
+    private static final String QUADRATIC_EQUATION = "^([+-]?\\d+|[+-]?\\d+\\.\\d+)x\\^2\\s([+-])\\s(\\d+|\\d+\\.\\d+)x\\s([+-])\\s(\\d+|\\d+\\.\\d+)\\s=\\s0$";
+    private static final String LINEAR_EQUATION = "^([+-]?\\d+|[+-]?\\d+\\.\\d+)x\\s([+-])\\s(\\d+|\\d+\\.\\d+)\\s=\\s0$";
+
+    public boolean parse(String s){
+        Pattern pattern = Pattern.compile(QUADRATIC_EQUATION);
+        Matcher matcher = pattern.matcher(s);
+
+        if (matcher.matches()){
+            return true;
+            /*
+                double a = Double.valueOf(matcher.group(1));
+                double b = Double.valueOf(matcher.group(2)+matcher.group(3));
+                double c = Double.valueOf(matcher.group(4)+matcher.group(5));
+                System.out.println("a=" + a + " b=" + b + " c=" + c);*/
+        }
+        else {
+            pattern = Pattern.compile(LINEAR_EQUATION);
+            matcher = pattern.matcher(s);
+            if (matcher.matches()) {
+                return true;
+                /*
+                double a = Double.valueOf(matcher.group(1));
+                double b = Double.valueOf(matcher.group(2) + matcher.group(3));
+                System.out.println("a=" + a + " b=" + b);*/
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
+        Parser parser = new Parser();
+        System.out.println(parser.parse("-4.1212x^2 - 0x - 16 = 0"));
 
+        /*parse("-4.1212x^2 - 0x - 16 = 0");
+        parse("-0.25x + 16 = 0");
+        parse("-4.1212x^2 - 0x - 16 = 5");
+        parse("- 0x - 16 = 0");*/
     }
 }
